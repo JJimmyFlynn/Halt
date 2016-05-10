2a89e4efa1dc47a678d9c39f1b3f66d5a2b1a990
John Flynn <john@catchfirecreative.com>
2016-05-10 09:46:13 -0400

    cleanup 404 template

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
+  
+  /**
+   * 404 Page Template
+   */
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
-</div>
-
-<?php get_search_form(); ?>
+ get_template_part('templates/page', 'header'); ?>
--- lib/setup.php
+++ lib/setup.php
@@ -11,10 +11,10 @@ function halt_setup() {
   // Enable features from Soil when plugin is activated
   // https://roots.io/plugins/soil/
   add_theme_support('soil-clean-up');
-  add_theme_support('soil-nav-walker');
   add_theme_support('soil-nice-search');
   add_theme_support('soil-jquery-cdn');
   add_theme_support('soil-relative-urls');
+  add_theme_support('soil-disable-asset-versioning');
 
   // Enable plugins to manage the document title
   // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
@@ -65,15 +65,21 @@ function display_sidebar() {
 }
 
 /**
- * Theme assets
+ * Enqueue theme assets
  */
 function assets() {
+  /**
+   * Enqueue theme css
+   */
   wp_enqueue_style('halt/css', Assets\asset_path('css/main.css'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
 
+  /**
+   * Enqueue theme javascript
+   */
   wp_enqueue_script('halt/js', Assets\asset_path('js/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
