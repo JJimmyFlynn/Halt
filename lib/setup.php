404116370d7def468653bef174e14c12a50821ac
John Flynn <john@catchfirecreative.com>
2016-06-22 14:50:27 -0400

    update asset enqueuing and manifest file

--- lib/setup.php
+++ lib/setup.php
@@ -71,7 +71,7 @@ function assets() {
   /**
    * Enqueue theme css
    */
-  wp_enqueue_style('halt/css', Assets\asset_path('css/main.css'), false, null);
+  wp_enqueue_style('halt/css', Assets\asset_path('styles/main.css'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
@@ -80,6 +80,6 @@ function assets() {
   /**
    * Enqueue theme javascript
    */
-  wp_enqueue_script('halt/js', Assets\asset_path('js/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('halt/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
