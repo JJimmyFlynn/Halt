cb84f090989f03d9b279f992840804ce1b0b7903
John Flynn <john@catchfirecreative.com>
2016-11-17 10:15:42 -0500

    update body class function to limit classes by default, change excerpt more filter markup

--- lib/extras.php
+++ lib/extras.php
@@ -5,19 +5,20 @@ namespace Halt\Extras;
 use Halt\Setup;
 
 /**
- * Add <body> classes
+ * Add allowed <body> classes
  */
 function body_class($classes) {
-  // Add page slug if it doesn't exist
-  if (is_single() || is_page() && !is_front_page()) {
-    if (!in_array(basename(get_permalink()), $classes)) {
-      $classes[] = basename(get_permalink());
-    }
-  }
+
+  $allowed_classes = [
+    'home',
+    'single',
+  ];
+
+  $classes = array_intersect($classes, $allowed_classes);
 
   // Add class if sidebar is active
   if (Setup\display_sidebar()) {
-    $classes[] = 'sidebar-primary';
+    $classes[] = 'has-sidebar';
   }
 
   return $classes;
@@ -28,6 +29,6 @@ add_filter('body_class', __NAMESPACE__ . '\\body_class');
  * Clean up the_excerpt()
  */
 function excerpt_more() {
-  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+  return ' &hellip; <a href="' . get_permalink() . '">Read More</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
