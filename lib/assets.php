563ff3bae9a9bbd3961fdab78a8858e07cbf4494
John Flynn <john@catchfirecreative.com>
2017-02-06 15:29:15 -0500

    simplyfy assets enqueues

--- lib/assets.php
+++ lib/assets.php
@@ -9,74 +9,11 @@ function assets() {
   /**
    * Enqueue theme css
    */
-  wp_enqueue_style('halt/css', asset_path('css/main.css'), false, null);
-
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
+  wp_enqueue_style('halt/css', halt_assets() . '/css/main.css', false, null);
 
   /**
    * Enqueue theme javascript
    */
-  wp_enqueue_script('halt/js', asset_path('js/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('halt/js', halt_assets() . '/js/main.js', null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
-
-/**
- * Get paths for assets
- */
-class JsonManifest {
-  private $manifest;
-
-  public function __construct($manifest_path) {
-    if (file_exists($manifest_path)) {
-      $this->manifest = json_decode(file_get_contents($manifest_path), true);
-    } else {
-      $this->manifest = [];
-    }
-  }
-
-  public function get() {
-    return $this->manifest;
-  }
-
-  public function getPath($key = '', $default = null) {
-    $collection = $this->manifest;
-    if (is_null($key)) {
-      return $collection;
-    }
-    if (isset($collection[$key])) {
-      return $collection[$key];
-    }
-    foreach (explode('.', $key) as $segment) {
-      if (!isset($collection[$segment])) {
-        return $default;
-      } else {
-        $collection = $collection[$segment];
-      }
-    }
-    return $collection;
-  }
-}
-
-/**
- * Determine asset path for enqueuing
- * @return String     Path to assets
- */
-function asset_path($filename) {
-  $dist_path = get_template_directory_uri() . '/dist/';
-  $directory = dirname($filename) . '/';
-  $file = basename($filename);
-  static $manifest;
-
-  if (empty($manifest)) {
-    $manifest_path = get_template_directory() . '/dist/' . 'assets.json';
-    $manifest = new JsonManifest($manifest_path);
-  }
-
-  if (array_key_exists($file, $manifest->get())) {
-    return $dist_path . $directory . $manifest->get()[$file];
-  } else {
-    return $dist_path . $directory . $file;
-  }
-}
