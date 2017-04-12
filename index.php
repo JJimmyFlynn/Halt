5cc9bbbc2950864bcaea3caa0f44c1d49bb33511
John Flynn <john@catchfirecreative.com>
2017-04-12 16:16:12 -0400

    clear out root index.php file

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 
 <?php
 
-// Make sure WP is looking for the theme in '/views' if it comes looking here
-if (defined('ABSPATH')) {
-    update_option('template', get_option('template') . '/views');
-}
+// No code here!
+// All layout files for the theme are located in the /views directory
