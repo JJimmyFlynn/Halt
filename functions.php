e853ee18712d433d90e3f579abcf6029c30c37a5
Nate Duffy <nate@catchfirecreative.com>
2017-08-21 10:44:09 -0400

    fix comment indent

--- functions.php
+++ functions.php
@@ -64,8 +64,8 @@ $halt_includes = [
   'lib/assets.php',          // Scripts and stylesheets
   'lib/setup.php',           // Theme setup
   'lib/utils.php',           // Theme utility toolbox
-  'lib/twig-functions.php',    // register Twig functions to use in templates
-  'lib/twig-filters.php',         // register Twig filters to use in templates
+  'lib/twig-functions.php',  // register Twig functions to use in templates
+  'lib/twig-filters.php',    // register Twig filters to use in templates
   'lib/timber.php'           // Timber setup and additions
 ];
 
