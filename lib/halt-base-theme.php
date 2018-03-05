9e3dd444444e3e8b648d7cebfb15c88765a86ff2
John Flynn <JJimmyFlynn@gmail.com>
2018-03-04 19:42:25 -0500

    disable soil search and relative urs by default

--- lib/halt-base-theme.php
+++ lib/halt-base-theme.php
@@ -111,8 +111,8 @@ abstract class HaltBaseTheme {
   */
   public function soil_theme_supports() {
     add_theme_support('soil-clean-up');
-    add_theme_support('soil-nice-search');
-    add_theme_support('soil-relative-urls');
+    // add_theme_support('soil-nice-search');
+    // add_theme_support('soil-relative-urls');
   }
 
   /**
