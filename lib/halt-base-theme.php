aa894ff3ebdc5eb6c6a44ce0d940ae40a332e7ab
John Flynn <JJimmyFlynn@gmail.com>
2018-03-12 15:55:49 -0400

    Add Soil JS to Footer to Base Theme Class

--- lib/halt-base-theme.php
+++ lib/halt-base-theme.php
@@ -111,6 +111,7 @@ abstract class HaltBaseTheme {
   */
   public function soil_theme_supports() {
     add_theme_support('soil-clean-up');
+    add_theme_support('soil-js-to-footer');
     // add_theme_support('soil-nice-search');
     // add_theme_support('soil-relative-urls');
   }
