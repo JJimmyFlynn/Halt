5456e9d4cbdfa6aba5e5b7245050c09d62998b72
John Flynn <JJimmyFlynn@gmail.com>
2018-04-24 21:18:52 -0400

    Remove erroneous comments

--- lib/halt-base-theme.php
+++ lib/halt-base-theme.php
@@ -82,18 +82,8 @@ abstract class HaltBaseTheme {
    * Add basic support theme supports
    */
   public function basic_supports() {
-    // Enable plugins to manage the document title
-    // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
     add_theme_support('title-tag');
-
-    // Enable post thumbnails
-    // http://codex.wordpress.org/Post_Thumbnails
-    // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
-    // http://codex.wordpress.org/Function_Reference/add_image_size
     add_theme_support('post-thumbnails');
-
-    // Enable HTML5 markup support
-    // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
     add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
 
     /**
